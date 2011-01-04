5f9e642bd510fb757617356f5b9d97696446dcd3
Luis Pedro Coelho <lpc@cmu.edu>
2011-01-04 13:10:35 -0500

    ENH: Load watch edges into graph

--- scripts/create_graph.py
+++ scripts/create_graph.py
@@ -1,5 +1,5 @@
 import tinygraph
-from gitpointer.models import User, FollowingRelationship
+from gitpointer.models import User, FollowingRelationship, WatchRelationship
 import gitpointer.backend
 
 TG = tinygraph.TinyGraph()
@@ -9,5 +9,10 @@ session = create_session()
 q = session.query(FollowingRelationship)
 for fr in q.yield_per(10):
     TG.add_edge(fr.follower_id, fr.followee_id)
+nusers = session.query(User).count()
+q = session.query(WatchRelationship)
+for wr in q.yield_per(10):
+    if wr.watched_repo is not None:
+        TG.add_edge(wr.watcher, wr.watched_repo+nusers)
 TG.finish()
 TG.dump('gitpointer.graph')
